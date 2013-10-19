b76ddda3328ea173d62d02245c2960ae20a89298
Tomek Straszewski <straszewski.tomasz@gmail.com>
2013-10-19 21:26:10 +0200

    Wzorzec builder, dynamiczne tworzenie planszy, wstępnie zaimplementowane dll dla warcab oraz szachów,

--- lab_2/Szachownica/Szachownica/Form1.Designer.cs
+++ lab_2/Szachownica/Szachownica/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelText = new System.Windows.Forms.Label();
+            this.gameComboBox = new System.Windows.Forms.ComboBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.labelText.Location = new System.Drawing.Point(14, 14);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(91, 13);
+            this.labelText.TabIndex = 0;
+            this.labelText.Text = "Choose game :";
+            this.labelText.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // gameComboBox
+            // 
+            this.gameComboBox.FormattingEnabled = true;
+            this.gameComboBox.Location = new System.Drawing.Point(12, 39);
+            this.gameComboBox.Name = "gameComboBox";
+            this.gameComboBox.Size = new System.Drawing.Size(121, 21);
+            this.gameComboBox.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(17, 80);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.AutoSize = true;
+            this.mainPanel.BackColor = System.Drawing.Color.Teal;
+            this.mainPanel.Controls.Add(this.labelText);
+            this.mainPanel.Controls.Add(this.gameComboBox);
+            this.mainPanel.Controls.Add(this.startButton);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(514, 334);
+            this.mainPanel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(514, 334);
+            this.Controls.Add(this.mainPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Board";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.ComboBox gameComboBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Panel mainPanel;
     }
 }
 
--- lab_2/Szachownica/Szachownica/Form1.cs
+++ lab_2/Szachownica/Szachownica/Form1.cs
@@ -26,5 +26,69 @@ namespace Szachownica
 
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.SetBounds(this.Left, this.Top, 200, 200);
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private static int SIZE = 50;
+
+        private void drawBoard()
+        {
+            Control[] c = this.Controls.Find("mainPanel", false);
+            Panel panel = c[0] as Panel;
+            panel.Controls.RemoveByKey("startButton");
+            panel.Controls.RemoveByKey("labelText");
+            panel.Controls.RemoveByKey("gameComboBox");
+
+            this.SetBounds(this.Left, this.Top, 465, 490);
+
+            bool white = true;
+            for (int y = 0; y < 9; y++)
+            {
+                if ((y % 2) == 0)
+                    white = true;
+                else
+                    white = false;
+                for (int x = 0; x < 9; x++)
+                {
+                    Button b = new Button();
+                    b.Width = SIZE;
+                    b.Height = SIZE;
+                    b.Top = y * SIZE;
+                    b.Left = x * SIZE;
+                    b.Margin = new Padding(0);
+
+                    if (white)
+                    {
+                        b.BackColor = Color.White;
+                    }
+                    else
+                    {
+                        b.BackColor = Color.Black;
+                    }
+
+                    white = !white;
+                    panel.Controls.Add(b);
+                }
+            }
+
+            this.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawBoard();
+
+
+        }
     }
 }

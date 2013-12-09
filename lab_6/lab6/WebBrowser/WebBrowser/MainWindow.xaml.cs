bdf56b528e99e47a934f9bd88fe04010afd13190
Tomek Straszewski <straszewski.tomasz@gmail.com>
2013-12-09 19:20:05 +0100

    Skończone

--- lab_6/lab6/WebBrowser/WebBrowser/MainWindow.xaml.cs
+++ lab_6/lab6/WebBrowser/WebBrowser/MainWindow.xaml.cs
@@ -24,5 +24,7 @@ namespace WebBrowser
         {
             InitializeComponent();
         }
+
+
     }
 }
--- lab_6/lab6/WebBrowser/WebBrowser/WPFWebBrowser.xaml.cs
+++ lab_6/lab6/WebBrowser/WebBrowser/WPFWebBrowser.xaml.cs
@@ -12,32 +12,63 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Controls;
+using System.ComponentModel;
 
 namespace WebBrowser
 {
     /// <summary>
     /// Interaction logic for WPFWebBrowser.xaml
     /// </summary>
-    public partial class WPFWebBrowser : UserControl
+    public partial class WPFWebBrowser : UserControl, INotifyPropertyChanged
     {
         public WPFWebBrowser()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
 
-        public static readonly DependencyProperty CurrentTimeProperty =
-        DependencyProperty.Register("HTML", typeof(String),
-         typeof(WPFWebBrowser), new PropertyMetadata(OnHTMLPropertyChanged));
 
-        public String HTML { get; set; }
+        public static readonly DependencyProperty HTMLStringProperty =
+        DependencyProperty.Register("HTMLString", typeof(String),
+         typeof(WPFWebBrowser), new PropertyMetadata(OnHTMLStringPropertyChanged));
 
-        public static void OnHTMLPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+
+        public string HTMLString
         {
-            Console.WriteLine("Odswierzam kontrolke");
+            get { return GetValue(HTMLStringProperty).ToString(); }
+            set
+            {
+                SetValue(HTMLStringProperty, value);
+            }
+        }
 
-            var mainWindow = new MainWindow();
-            //mainWindow.WebBrowserControl.se
+        public static void OnHTMLStringPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WPFWebBrowser control = d as WPFWebBrowser;
+            control.OnPropertyChanged("HTMLString");
+            control.changeContent();
+        }
+
+        private void changeContent()
+        {
+            //zmieniam zawartosc przegladarki
+            webBrowserComponent.NavigateToString(HTMLString);
+        }
+
+        private void OnHTMLPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            webBrowserComponent.NavigateToString(HTMLString);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string html)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(html));
+            }
         }
 
     }
